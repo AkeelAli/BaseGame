8923e70e9362cad848792f16c9adf9fc48f64240
Akeel <agilowen@gmail.com>
2014-07-12 01:27:53 -0400

    first commit of the prototype with preliminary interface work

---
+++ src/com/example/basegame/Gameplay.java
@@ -0,0 +1,15 @@
+package com.example.basegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Gameplay extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gameplay);
+	}
+}
